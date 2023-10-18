b2e66da472e95666156034816ada5eaea8178717
mchoros <130072236+mchoros@users.noreply.github.com>
2023-10-18 12:08:53 +0200

    Add files via upload

---
+++ src/main/java/pwo/LucasGenerator.java
@@ -0,0 +1,26 @@
+package pwo;
+
+import java.util.Scanner;
+public class LucasGenerator {
+   public static void main(String[] args) { 
+
+     int a=2,b=1,c,i;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length of lucas series:");
+		int len=sc.nextInt();
+		for(i=1;i<=len;i++){
+		    c=a+b;
+		    System.out.print(c+" ");
+		    a=b;
+		    b=c;
+    }
+ }
+
+    LucasGenerator() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getTerm(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
